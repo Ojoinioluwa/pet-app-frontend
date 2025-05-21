3e5ca5b901d32de484d2d36160a0ec468165ac6c
Ojay <ojoinioluwa@gmail.com>
2025-05-22 00:48:47 +0100

    Fix the error tht occured when building

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
 // const BASE_URL = "http://192.168.16.178:5000/api/v1";
-const BASE_URL = "https://petapp-backend-pxx1.onrender.com/api/";
+const BASE_URL = "https://petapp-backend-pxx1.onrender.com/api/v1";
 
 export default BASE_URL

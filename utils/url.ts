e8cb3466127f1cda977e5548ea74bc4b1e4f4046
Ojay <ojoinioluwa@gmail.com>
2025-05-21 15:05:10 +0100

    Modify the BASE_URL

--- utils/url.ts
+++ utils/url.ts
@@ -1,3 +1,4 @@
-const BASE_URL = "http://192.168.16.178:5000/api/v1";
+// const BASE_URL = "http://192.168.16.178:5000/api/v1";
+const BASE_URL = "https://petapp-backend-pxx1.onrender.com/api/";
 
 export default BASE_URL
